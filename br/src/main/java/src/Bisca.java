097b3f6b494c9148d9f3f47cbbb7a049ca6d5d6f
RianVitor26 <rianvitorlhe@gamil.com>
2022-12-11 14:56:05 -0300

    Criando Jogadores

--- br/src/main/java/src/Bisca.java
+++ br/src/main/java/src/Bisca.java
@@ -4,8 +4,7 @@ package src;
  * Hello world!
  *
  */
-public class Bisca 
-{
+public class Bisca {
     public static void main( String[] args )
     {
         Map<String, Integer> value = new TreeMap<>();
@@ -19,8 +18,8 @@ public class Bisca
         value.put("K", 4);
         value.put("A", 11);
         value.put("7", 10);
-    }
-    String[] nipes = { " Copas", " Espada", " Ouros", " Paus" };
+
+        String[] nipes = { " Copas", " Espada", " Ouros", " Paus" };
         List<String> cartas = new ArrayList<String>();
         String carta;
         for (int i = 0; i < 4; i++) {
@@ -29,4 +28,25 @@ public class Bisca
                 cartas.add(carta);
             }
         }
+
+        List<String> Jogador1 = new ArrayList<>();
+
+        /// método inicia game
+        Random rand = new Random();
+        for (int j = 0; j < 3; j++) {
+            String carta_jogador = cartas.get(rand.nextInt(cartas.size()));
+            Jogador1.add(carta_jogador);
+            cartas.remove(carta_jogador);
+        }
+
+        List<String> Jogador2 = new ArrayList<>();
+
+        for (int j = 0; j < 3; j++) {
+            String carta_jogador = cartas.get(rand.nextInt(cartas.size()));
+            Jogador2.add(carta_jogador);
+            cartas.remove(carta_jogador);
+        }
+    }
+    
+        
 }
